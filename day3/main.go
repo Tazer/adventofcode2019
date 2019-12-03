b6d57faa7ecca00dc0fd7bf2bd16ea1aa22594aa
Patrik Potocki <rambo3swe@gmail.com>
2019-12-03 16:51:12 +0100

    day3 complete

--- day3/main.go
+++ day3/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bufio"
 	"log"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -31,8 +34,165 @@ func main() {
 
 	log.Printf("Line1: %s Line2: %s", line1, line2)
 
+	res, steps, err := calculateManhattanDistance(line1, line2)
+
+	if err != nil {
+		log.Fatalf("Problemo err:%v", err)
+	}
+
+	log.Printf("The result is %d and steps %d 🥳", res, steps)
 }
 
-func calculateManhattanDistance(line1,line2 string) int {
-	return 0
+type Point struct {
+	X     int
+	Y     int
+	Steps int
+}
+
+func calculateManhattanDistance(line1, line2 string) (int, int, error) {
+	l1i := strings.Split(line1, ",")
+	l2i := strings.Split(line2, ",")
+
+	line1Points := []Point{Point{
+		X:     0,
+		Y:     0,
+		Steps: 0,
+	}}
+
+	line2Points := []Point{Point{
+		X:     0,
+		Y:     0,
+		Steps: 0,
+	}}
+
+	for _, l := range l1i {
+		direction := string(l[0])
+		value, err := strconv.Atoi(l[1:])
+
+		if err != nil {
+			return 0, 0, err
+		}
+
+		var currentPoint Point
+
+		if len(line1Points) > 0 {
+			currentPoint = line1Points[len(line1Points)-1]
+		}
+
+		newPoint := processMovement(direction, value, currentPoint)
+
+		for _, p := range newPoint {
+			line1Points = append(line1Points, p)
+		}
+	}
+
+	for _, l := range l2i {
+		direction := string(l[0])
+		value, err := strconv.Atoi(l[1:])
+
+		if err != nil {
+			return 0, 0, err
+		}
+
+		var currentPoint Point
+
+		if len(line2Points) > 0 {
+			currentPoint = line2Points[len(line2Points)-1]
+		}
+
+		newPoint := processMovement(direction, value, currentPoint)
+
+		for _, p := range newPoint {
+			line2Points = append(line2Points, p)
+		}
+
+	}
+
+	matches := []map[string]Point{}
+
+	var shortest []Point
+	var longest []Point
+
+	if len(line1Points) > len(line2Points) {
+		longest = line1Points
+		shortest = line2Points
+	} else {
+		shortest = line1Points
+		longest = line2Points
+	}
+
+	for _, l1 := range longest {
+		for _, l2 := range shortest {
+			if l1.X == l2.X && l1.Y == l2.Y {
+				m := map[string]Point{}
+				m["l1"] = l1
+				m["l2"] = l2
+				matches = append(matches, m)
+			}
+		}
+	}
+
+	log.Printf("matches points %d  , %+v", len(matches), matches)
+
+	distance := 0.0
+	steps := 0
+
+	for _, m := range matches {
+
+		d := math.Abs(float64(m["l1"].X-0)) + math.Abs(float64(m["l1"].Y-0))
+
+		if distance > d || distance == 0 {
+			distance = d
+			log.Printf("steps %d", steps)
+
+		}
+
+		cSteps := 0
+
+		for k, v := range m {
+			log.Printf("k: %s , steps: %d", k, v.Steps)
+			cSteps += v.Steps
+		}
+
+		if steps > cSteps || steps == 0 {
+			steps = cSteps
+		}
+
+	}
+
+	return int(distance), steps, nil
+}
+
+func processMovement(direction string, value int, current Point) []Point {
+	processPoints := []Point{}
+
+	if direction == "U" {
+		for i := 1; i <= value; i++ {
+			current.Steps++
+			processPoints = append(processPoints, Point{X: current.X, Y: current.Y + i, Steps: current.Steps})
+		}
+	}
+
+	if direction == "D" {
+		for i := 1; i <= value; i++ {
+			current.Steps++
+			processPoints = append(processPoints, Point{X: current.X, Y: current.Y - i, Steps: current.Steps})
+		}
+	}
+
+	if direction == "L" {
+		for i := 1; i <= value; i++ {
+			current.Steps++
+			processPoints = append(processPoints, Point{X: current.X - i, Y: current.Y, Steps: current.Steps})
+		}
+	}
+
+	if direction == "R" {
+		for i := 1; i <= value; i++ {
+			current.Steps++
+			processPoints = append(processPoints, Point{X: current.X + i, Y: current.Y, Steps: current.Steps})
+		}
+	}
+
+	return processPoints
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -3,8 +3,33 @@ package main
 import (
 	"testing"
 
-	"gopkg.in/go-playground/assert.v1"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestCrossinglinesBasic(t *testing.T) {
+	cases := []struct {
+		name     string
+		line1    string
+		line2    string
+		expected int
+	}{
+		{
+			name:     "example 1",
+			line1:    "R8,U5,L5,D3",
+			line2:    "U7,R6,D4,L4",
+			expected: 6,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got, _, err := calculateManhattanDistance(c.line1, c.line2)
+			assert.Nil(t, err)
+			assert.Equal(t, c.expected, got)
+		})
+
+	}
+}
 
 func TestCrossinglines(t *testing.T) {
 	cases := []struct {
@@ -35,8 +60,64 @@ func TestCrossinglines(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			got := calculateManhattanDistance(c.line1, c.line2)
+			got, _, err := calculateManhattanDistance(c.line1, c.line2)
+			assert.Nil(t, err)
+			assert.Equal(t, c.expected, got)
+		})
 
+	}
+}
+
+func TestCrossinglinesSimple(t *testing.T) {
+	cases := []struct {
+		name     string
+		line1    string
+		line2    string
+		expected int
+	}{
+		{
+			name:     "example 1",
+			line1:    "R2,U2,D2",
+			line2:    "U2,R8",
+			expected: 6,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got, _, err := calculateManhattanDistance(c.line1, c.line2)
+			assert.Nil(t, err)
+			assert.Equal(t, c.expected, got)
+		})
+
+	}
+}
+
+func TestSteps(t *testing.T) {
+	cases := []struct {
+		name     string
+		line1    string
+		line2    string
+		expected int
+	}{
+		{
+			name:     "example 1",
+			line1:    "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+			line2:    "U62,R66,U55,R34,D71,R55,D58,R83",
+			expected: 610,
+		},
+		{
+			name:     "example 1",
+			line1:    "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+			line2:    "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+			expected: 410,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			_, got, err := calculateManhattanDistance(c.line1, c.line2)
+			assert.Nil(t, err)
 			assert.Equal(t, c.expected, got)
 		})
 
