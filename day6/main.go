19587cee589ebe38c68e698a7a63eac5118082ac
Patrik Potocki <rambo3swe@gmail.com>
2019-12-09 17:39:44 +0100

    doing some stuff

--- day6/main.go
+++ day6/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,5 +21,43 @@ func main() {
 }
 
 func calculateOrbitConnections(input []string) int {
-	return 0
+
+	connections := 0
+
+	for i := len(input) - 1; i >= 0; i-- {
+		r := strings.Split(strings.TrimSpace(input[i]), ")")
+		//a := r[0]
+		b := strings.TrimSpace(r[0])
+
+		connections++
+
+		// log.Printf("direct %v", r)
+
+		offset := 0
+
+		next := i - 1
+
+		for ii := next; ii >= 0; ii-- {
+
+			r := strings.Split(input[ii], ")")
+
+			for iii := next - offset; iii >= 0; iii-- {
+
+				r := strings.Split(input[iii], ")")
+
+				log.Printf("checking if %s is %s", r[1], b)
+
+				if strings.TrimSpace(r[1]) == b {
+					log.Printf("indirect %v", r)
+					connections++
+				}
+			}
+
+			b = strings.TrimSpace(r[0])
+			offset = 1
+		}
+
+	}
+
+	return connections
 }
